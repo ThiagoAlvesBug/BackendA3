91e7e77f82acdc734bf8fa03844bc99e83c2a6ff
Thiago Alves Bugueño <133525166+ThiagoAlvesBug@users.noreply.github.com>
2025-11-21 21:05:32 -0300

    F U N C I O N A N D O

--- auth-service/src/main/java/com/ucdual/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/ucdual/auth_service/config/SecurityConfig.java
@@ -18,12 +18,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/register", "/auth/login").permitAll()
-                .anyRequest().authenticated()
-            );
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
     }
+
 }
